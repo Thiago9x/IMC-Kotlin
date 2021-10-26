47255b03bb2d0a5130c5bed082ca9dcc8d698ff6
Thiago9x <thimendonca44@gmail.com>
2021-10-26 16:53:06 -0300

    dashboard completo cadastro inciado

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cadastro)
 
-        supportActionBar!!.hide()
+//        supportActionBar!!.hide()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_cadastro,menu)
+        return true
     }
 }
