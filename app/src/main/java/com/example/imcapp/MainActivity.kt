6310ed7e5b94a63b9f02c215c7ea9e294e8ea37d
Thiago9x <thimendonca44@gmail.com>
2021-11-09 16:51:48 -0300

    all

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.imcapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_main)
 
-//        supportActionBar!!.hide()
+        //excluir a toolbar
+        supportActionBar!!.hide()
+
+        //instanciar o botao criar conta
+        val botaoNovoUsuario = findViewById<Button>(R.id.btn_criar_conta)
+
+        //Criar um listener/ouvinte
+        botaoNovoUsuario.setOnClickListener {
+        //criar uma intent
+            val x = "10"
+            val abrirCadastro = Intent(this, Cadastro::class.java)
+            startActivity(abrirCadastro)
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_cadastro,menu)
-        return true
-    }
+
 }
