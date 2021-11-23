1402b215dda43487ecf2ca77a0aee40cf9205d86
Thiago9x <thimendonca44@gmail.com>
2021-11-23 15:44:54 -0300

    all

---
+++ app/src/main/java/com/example/imcapp/Pesagem.kt
@@ -0,0 +1,12 @@
+package com.example.imcapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Pesagem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesagem)
+    }
+}
+
